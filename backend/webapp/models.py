4e8316107ddc7363b1747de083002efbf4e04780
jlee-3 <leejhnthn93@gmail.com>
2023-03-26 14:09:03 +0800

    fix(expense model): more appropriate naming and types

--- backend/webapp/models.py
+++ backend/webapp/models.py
@@ -4,16 +4,16 @@ from django.db import models
 
 
 class Expense(models.Model):
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3)
     category = models.CharField(max_length=30)
-    createdAt = models.DateField()
-    updatedAt = models.DateField()
+    createdAt = models.DateTimeField()
+    updatedAt = models.DateTimeField()
 
     class Meta:
         verbose_name = 'expense'
         verbose_name_plural = 'expenses'
 
     def __str__(self):
-        return self.name
+        return self.title
