8a199b300327686481e21012cb15a674a5485a7b
jlee-3 <leejhnthn93@gmail.com>
2023-04-26 15:45:13 +0800

    feat(database): add subCategory, use description instead of title

--- backend/webapp/models.py
+++ backend/webapp/models.py
@@ -7,12 +7,13 @@ import uuid
 class Expense(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=60)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     spotRate = models.DecimalField(
         max_digits=10, decimal_places=2, default=None, blank=True, null=True)
     currency = models.CharField(max_length=3)
-    category = models.CharField(max_length=30)
+    category = models.CharField(max_length=30, null=True)
+    subCategory = models.CharField(max_length=60, null=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now_add=True)
 
